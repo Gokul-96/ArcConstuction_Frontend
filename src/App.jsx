2cbd3944b01d0ca1ee56058112f7587ec56529d7
gokul <gokulakrishnan.m96@gmail.com>
2025-06-05 20:23:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import HomeComponent from './Components/HomeComponent'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <HomeComponent/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/HomeComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import image1 from '../assets/image1.jpg'
+
+const HomeComponent = () => {
+  return (
+    <div>
+        <img src={image1} alt="Home" className='home-image'/>
+    </div>
+    
+  )
+}
+
+export default HomeComponent
